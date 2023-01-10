418563b23dc5e017152cafc95413d7689d96930d
Rogerio Azevedo <rogerio@rogerioazevedo.net>
2023-01-09 20:02:28 -0400

    feat: change image on heroHome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,26 @@
-import React from 'react';
+import React from "react"
 
-import Header from '../partials/Header';
-import PageIllustration from '../partials/PageIllustration';
-import HeroHome from '../partials/HeroHome';
-import Stats from '../partials/Stats';
-import Carousel from '../partials/Carousel';
-import Tabs from '../partials/Tabs';
-import Process from '../partials/Process';
-import PricingTables from '../partials/PricingTables';
-import TestimonialsBlocks from '../partials/TestimonialsBlocks';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
-import Cta from '../partials/Cta';
-import Footer from '../partials/Footer';
+import Header from "../partials/Header"
+import PageIllustration from "../partials/PageIllustration"
+import HeroHome from "../partials/HeroHome"
+import Stats from "../partials/Stats"
+import TestimonialsBlocks from "../partials/TestimonialsBlocks"
+import FeaturesBlocks from "../partials/FeaturesBlocks"
+import Footer from "../partials/Footer"
 
 function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-
       {/*  Site header */}
       <Header />
 
       {/*  Page content */}
       <main className="grow">
-
         {/*  Page illustration */}
-        <div className="relative max-w-6xl mx-auto h-0 pointer-events-none -z-1" aria-hidden="true">
+        <div
+          className="relative max-w-6xl mx-auto h-0 pointer-events-none -z-1"
+          aria-hidden="true"
+        >
           <PageIllustration />
         </div>
 
@@ -38,14 +34,12 @@ function Home() {
         <TestimonialsBlocks />
         <FeaturesBlocks />
         {/* <Cta /> */}
-
       </main>
 
       {/*  Site footer */}
       <Footer />
-
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
